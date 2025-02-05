38183fef48c51c6f8a714987d200b2b6f9a1eee4
harshith0912 <harshithrao088@gmail.com>
2025-02-05 12:00:46 +0530

    day1

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//       <section>
+//       <h1 style={{color:"red"}}>Hello</h1>
+//       <p style={{color:"red", fontSize:"25px"}}>World</p>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Sidebar1 from "./components/Sidebar1";
+import Sidebar2 from "./components/Sidebar2";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Main/>
+      <div className='sidebar'>
+      <Sidebar1/>
+      <Sidebar2/>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
